8aadb7e63f59475ca678e2d3a89cae3cac1e99b9
yelamin2 <yelamin2@yahoo.com>
2022-12-06 23:41:48 -0500

    added categories to articles

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.conf import settings
 
+CATEGORIES= (
+    ("Politics", "Politics"),
+    ("Lifestyle", "Lifestyle"),
+    ("Sports", "Sports"),
+    ("Business", "Business"),
+)
+
+# class Category(models.Model):
+#     category=models.CharField(
+#         max_length=20,
+#         choices= CATEGORIES,
+#         )
+
 
 class Article(models.Model):
     author = models.ForeignKey(
@@ -10,8 +23,13 @@ class Article(models.Model):
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now= True)
     is_published = models.BooleanField(default=False)
+    category=models.CharField(
+        max_length=20,
+        choices= CATEGORIES,
+        )
     image = models.ImageField(
         upload_to='articles/',default='articles/default.jpeg')
+    
 
     def __str__(self):
         return self.title
--- articles/serializers.py
+++ articles/serializers.py
@@ -6,3 +6,9 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+
+    
+# class CategorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model =Category
+#         fields= '__all__'
